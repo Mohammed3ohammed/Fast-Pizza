e817600e1bc1912223e52a579132eb8a98208989
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-05-19 07:57:16 +0300

    fix: add return statement in Header component to render the header properly

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-    <header>
-        <h1>Fast</h1>
+    return (
+        <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+        <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+        </Link>
     </header>
+    )
 }
 
 export default Header;
