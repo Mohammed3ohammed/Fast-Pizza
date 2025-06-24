4ea62682b3764403d228ca51fc8887ce3b2e4507
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-06-24 21:24:31 +0300

    Configure Redux store with user and cart reducers

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Home from './ui/Home';
 import Error from './ui/Error';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder, {
   action as createOrderAction,
